a12fc2b724e65b9c0f6849f95fecb5a77f140f07
christian.fohrmann <christian@alpinmarketing.at>
2025-07-01 14:36:57 +0200

    v6.0

--- am-hotelfolio-reguest.php
+++ am-hotelfolio-reguest.php
@@ -3,7 +3,7 @@
  * Plugin Name: AM Hotelfolio Reguest
  * Plugin URI: https://www.web-crossing.com
  * Description: Sends Contact Form 7 Fields to Reguest
- * Version: 5.0
+ * Version: 6.0
  * Author: Ing. Christian Fohrmann
  * Author URI: https://www.alpinmarketing.at
  */
